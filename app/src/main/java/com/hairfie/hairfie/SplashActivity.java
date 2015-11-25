03e571ed4944c3b1507003fcea4f96c6421e9ef3
Stéphane JAIS <stephane@cantinasoftware.com>
2015-11-25 19:00:43 +0100

    blank activities.

---
+++ app/src/main/java/com/hairfie/hairfie/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.hairfie.hairfie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hairfie.hairfie.models.User;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, User.getCurrentUser().isAuthenticated() ? MainActivity.class : IntroActivity.class);
+                startActivity(intent);
+
+            }
+        }, 1000);
+    }
+
+
+}
