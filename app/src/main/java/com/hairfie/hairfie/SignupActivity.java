6b4bc839b696450f27dad61fceb36fb2c4f3c22f
Stéphane JAIS <stephane@cantinasoftware.com>
2015-11-26 14:55:41 +0100

    validate button

--- app/src/main/java/com/hairfie/hairfie/SignupActivity.java
+++ app/src/main/java/com/hairfie/hairfie/SignupActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
@@ -16,6 +18,7 @@ public class SignupActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
 }
