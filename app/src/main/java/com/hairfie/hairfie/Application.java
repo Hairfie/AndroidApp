0798b81edccf2ff2e8bf3ab974617cf021dbab75
Stéphane JAIS <stephane@cantinasoftware.com>
2015-12-04 09:32:18 +0100

    location services.

--- app/src/main/java/com/hairfie/hairfie/Application.java
+++ app/src/main/java/com/hairfie/hairfie/Application.java
@@ -1,13 +1,19 @@
 package com.hairfie.hairfie;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.facebook.FacebookSdk;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.hairfie.hairfie.models.Category;
 import com.hairfie.hairfie.models.ResultCallback;
 import com.hairfie.hairfie.models.User;
@@ -15,13 +21,17 @@ import com.squareup.picasso.LruCache;
 import com.squareup.picasso.Picasso;
 
 import pl.aprilapps.easyphotopicker.EasyImage;
+
 import com.squareup.picasso.OkHttpDownloader;
+
 /**
  * Created by stephh on 24/11/15.
  */
-public class Application extends android.app.Application {
+public class Application extends android.app.Application implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    public static final String LOCATION_UPDATED_BROADCAST_INTENT = "LOCATION_UPDATED_BROADCAST_INTENT";
     public static final String TAG = "Hairfie";
     private static Picasso sPicasso;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -33,7 +43,6 @@ public class Application extends android.app.Application {
 //        sPicasso.setIndicatorsEnabled(true);
 //        sPicasso.setLoggingEnabled(true);
         Picasso.setSingletonInstance(sPicasso);
-
 
 
         // Easy image configuration (for profile pictures)
@@ -56,6 +65,9 @@ public class Application extends android.app.Application {
 
         // Prepare category list
         Category.fetchAll(null);
+
+        // Google apis
+        buildGoogleApiClient();
     }
 
     @NonNull
@@ -78,4 +90,64 @@ public class Application extends android.app.Application {
         return LocalBroadcastManager.getInstance(getInstance());
     }
 
+    Location mLastLocation;
+
+    public Location getLastLocation() {
+        return mLastLocation;
+    }
+
+    public void updateLastLocation() {
+        if (mConnectedToGoogleApi) {
+            Location newLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+            boolean updated;
+            if (null == mLastLocation && null != newLastLocation)
+                updated = true;
+            else if (null != mLastLocation && null != newLastLocation && !newLastLocation.equals(mLastLocation))
+                updated = true;
+            else
+                updated = false;
+
+            if (null != newLastLocation)
+                mLastLocation = newLastLocation;
+            
+            if (updated) {
+                getBroadcastManager().sendBroadcast(new Intent(LOCATION_UPDATED_BROADCAST_INTENT));
+            }
+
+        }
+    }
+
+    GoogleApiClient mGoogleApiClient;
+    boolean mConnectedToGoogleApi;
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mConnectedToGoogleApi = true;
+        
+        // Immediately request location to get a first fix
+        if (null == mLastLocation)
+            updateLastLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(Application.TAG, "Google APIs connection suspended");
+        mConnectedToGoogleApi = false;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        mConnectedToGoogleApi = false;
+        CharSequence message = connectionResult.getErrorMessage();
+        Log.d(Application.TAG, "Could not connect to Google APIs: " + (null != message ? message : "null"));
+    }
 }
