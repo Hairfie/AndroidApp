a4605b48f86072f94f91f3294523e44a24f6903b
Stéphane JAIS <stephane@cantinasoftware.com>
2015-12-09 09:42:14 +0100

    new fragment.

---
+++ app/src/main/java/com/hairfie/hairfie/BusinessMapFragment.java
@@ -0,0 +1,10 @@
+package com.hairfie.hairfie;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Created by stephh on 09/12/15.
+ */
+public class BusinessMapFragment extends SupportMapFragment {
+
+}
