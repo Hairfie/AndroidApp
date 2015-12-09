4892f83b812b2fc030900f9975bd3efb8d2e3259
Stéphane JAIS <stephane@cantinasoftware.com>
2015-12-09 10:31:44 +0100

    results: using a view pager.

--- app/src/main/java/com/hairfie/hairfie/SearchResultsActivity.java
+++ app/src/main/java/com/hairfie/hairfie/SearchResultsActivity.java
@@ -1,23 +1,22 @@
 package com.hairfie.hairfie;
 
 import android.app.SearchManager;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.hairfie.hairfie.models.Business;
 import com.hairfie.hairfie.models.Category;
@@ -34,13 +33,21 @@ public class SearchResultsActivity extends AppCompatActivity {
     List<Category> mCategories;
     String mQuery;
     GeoPoint mGeoPoint;
-    private Layout mContainer;
+    private ViewPager mContainer;
+
+    BusinessMapFragment mMapFragment;
+    BusinessListFragment mListFragment;
     private BusinessRecyclerViewAdapter mAdapter = new BusinessRecyclerViewAdapter(new BusinessListFragment.OnListFragmentInteractionListener() {
         @Override
         public void onListFragmentInteraction(Business item) {
             onTouchBusiness(item);
         }
-    });
+
+
+    }
+    );
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +60,9 @@ public class SearchResultsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        BusinessMapFragment mapFragment = (BusinessMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.setAdapter(mAdapter);
-        mapFragment.setInteractionListener(new BusinessMapFragment.OnMapFragmentInteractionListener() {
+        mMapFragment = new BusinessMapFragment();
+        mMapFragment.setAdapter(mAdapter);
+        mMapFragment.setInteractionListener(new BusinessMapFragment.OnMapFragmentInteractionListener() {
 
             @Override
             public void onMapFragmentInteraction(Business item) {
@@ -66,8 +72,8 @@ public class SearchResultsActivity extends AppCompatActivity {
 
         handleIntent(getIntent());
 
-        BusinessListFragment businessListFragment = (BusinessListFragment) getSupportFragmentManager().findFragmentById(R.id.results);
-        businessListFragment.setAdapter(mAdapter);
+        mListFragment = new BusinessListFragment();
+        mListFragment.setAdapter(mAdapter);
 
         mAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
@@ -76,8 +82,9 @@ public class SearchResultsActivity extends AppCompatActivity {
                 mContainer.setVisibility(View.VISIBLE);
             }
         });
-        mContainer = (Layout)findViewById(R.id.container);
+        mContainer = (ViewPager)findViewById(R.id.container);
         mContainer.setVisibility(View.INVISIBLE);
+        mContainer.setAdapter(mPagerAdapter);
 
     }
 
@@ -179,76 +186,30 @@ public class SearchResultsActivity extends AppCompatActivity {
     }
 
 
-    public static class Layout extends ViewGroup {
-
-        Button mButton;
-        RecyclerView mRecyclerView;
-        View mMapView;
-        public Layout(Context context, AttributeSet attrs) {
-            super(context, attrs);
-        }
-
-        private int dY = 0;
-        @Override
-        public void addView(View child, int index, LayoutParams params) {
-            super.addView(child, index, params);
-            if (child instanceof Button)
-                mButton = (Button)child;
-            else if (child instanceof RecyclerView) {
-                mRecyclerView = (RecyclerView) child;
-                mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-                    @Override
-                    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                        super.onScrollStateChanged(recyclerView, newState);
-                    }
-
-                    @Override
-                    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                        super.onScrolled(recyclerView, dx, dy);
-
-                        float buttonTop = mButton.getTop() + mButton.getTranslationY();
-                        if (buttonTop - dy < 0)
-                            dy = Math.round(buttonTop);
-
-                        float mapViewTop =  mMapView.getTop() + mMapView.getTranslationY();
-                        if (mapViewTop - dy > 0)
-                            dy = Math.round(mapViewTop);
-
-                        mButton.setTranslationY(mButton.getTranslationY() - dy);
-                        mMapView.setTranslationY(mMapView.getTranslationY()-dy);
-                    }
-                });
-            } else
-                mMapView = child;
-        }
+    FragmentPagerAdapter mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
 
         @Override
-        protected void onLayout(boolean changed, int l, int t, int r, int b) {
-            if (null == mButton || null == mRecyclerView || null == mMapView) {
-                Log.e(Application.TAG, "SearchResultsActivity$Layout Refusing to layout as it is incomplete.");
-                return;
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return mListFragment;
             }
-            LayoutParams buttonLayoutParams = mButton.getLayoutParams();
-            int desiredButtonHeight =  buttonLayoutParams.height;
-
-
-
-            int height = getMeasuredHeight();
-            int thirdOfHeight = Math.round((float) height / 3.0f);
-            int desiredMapHeight = thirdOfHeight;
-            int width = getMeasuredWidth();
-
-            mMapView.measure(MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
-                    MeasureSpec.makeMeasureSpec(desiredMapHeight, MeasureSpec.EXACTLY));
-            mButton.measure(MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
-                    MeasureSpec.makeMeasureSpec(desiredButtonHeight, MeasureSpec.EXACTLY));
-            mRecyclerView.measure(MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
-                    MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
-
-            mMapView.layout(0,0,width,desiredMapHeight);
-            mButton.layout(0, desiredMapHeight, width, desiredMapHeight + desiredButtonHeight);
-            mRecyclerView.layout(0, 0, width, height);
-            mRecyclerView.setPadding(0, desiredButtonHeight + desiredMapHeight, 0, 0);
+            return mMapFragment;
         }
-    }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.results);
+                case 1:
+                    return getString(R.string.map);
+            }
+            return null;
+        }
+
+    };
 }
