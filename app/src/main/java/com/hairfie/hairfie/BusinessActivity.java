e941c7377eafc411bf7863d11f125c72f8acb3d6
Stéphane JAIS <stephane@cantinasoftware.com>
2015-12-11 12:08:38 +0100

    call business.

--- app/src/main/java/com/hairfie/hairfie/BusinessActivity.java
+++ app/src/main/java/com/hairfie/hairfie/BusinessActivity.java
@@ -1,9 +1,14 @@
 package com.hairfie.hairfie;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -11,11 +16,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -38,8 +45,9 @@ import java.util.Locale;
 public class BusinessActivity extends AppCompatActivity implements BusinessInfoFragment.OnFragmentInteractionListener, HairfieGridFragment.OnHairfieGridFragmentInteractionListener {
 
     public static final String EXTRA_BUSINESS = "EXTRA_BUSINESS";
-
+    private static final int CALL_PERMISSION_REQUEST = 101;
     Business mBusiness;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,7 +58,7 @@ public class BusinessActivity extends AppCompatActivity implements BusinessInfoF
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
-        mBusiness = null != intent ? (Business)intent.getParcelableExtra(EXTRA_BUSINESS) : null;
+        mBusiness = null != intent ? (Business) intent.getParcelableExtra(EXTRA_BUSINESS) : null;
         if (null == mBusiness) {
             Log.e(Application.TAG, "Null business");
             finish();
@@ -70,7 +78,7 @@ public class BusinessActivity extends AppCompatActivity implements BusinessInfoF
         }
 
         // Num reviews
-        TextView numReviews = (TextView)findViewById(R.id.num_reviews);
+        TextView numReviews = (TextView) findViewById(R.id.num_reviews);
         if (null != numReviews) {
             if (mBusiness.numReviews != 1) {
                 numReviews.setText(String.format(Locale.getDefault(), getString(R.string.x_reviews), mBusiness.numReviews));
@@ -80,21 +88,21 @@ public class BusinessActivity extends AppCompatActivity implements BusinessInfoF
         }
 
         // Name
-        TextView nameTextView = (TextView)findViewById(R.id.business_name);
+        TextView nameTextView = (TextView) findViewById(R.id.business_name);
         if (null != nameTextView)
             nameTextView.setText(mBusiness.name);
 
-        final TextView name2TextView = (TextView)findViewById(R.id.business_name2);
+        final TextView name2TextView = (TextView) findViewById(R.id.business_name2);
         if (null != name2TextView)
             name2TextView.setText(mBusiness.name);
 
-        AppBarLayout appBarLayout = (AppBarLayout)findViewById(R.id.appbar);
-        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        final CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
 
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                if(collapsingToolbar.getHeight() + verticalOffset < 2 * ViewCompat.getMinimumHeight(collapsingToolbar)) {
+                if (collapsingToolbar.getHeight() + verticalOffset < 2 * ViewCompat.getMinimumHeight(collapsingToolbar)) {
                     name2TextView.animate().alpha(1).setDuration(600);
                 } else {
                     name2TextView.animate().alpha(0).setDuration(600);
@@ -112,14 +120,17 @@ public class BusinessActivity extends AppCompatActivity implements BusinessInfoF
         final BusinessReviewsFragment reviewsFragment = BusinessReviewsFragment.newInstance(mBusiness);
         final HairfieGridFragment hairfiesFragment = HairfieGridFragment.newInstance(2, mBusiness);
         // View pager
-        ViewPager viewPager = (ViewPager)findViewById(R.id.view_pager);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
         viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
                 switch (position) {
-                    case 0: return infoFragment;
-                    case 1: return reviewsFragment;
-                    case 2: return hairfiesFragment;
+                    case 0:
+                        return infoFragment;
+                    case 1:
+                        return reviewsFragment;
+                    case 2:
+                        return hairfiesFragment;
                 }
                 return null;
             }
@@ -132,15 +143,22 @@ public class BusinessActivity extends AppCompatActivity implements BusinessInfoF
         });
 
 
-        TabLayout tabs = (TabLayout)findViewById(R.id.tabs);
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         for (int i = 0; i < tabs.getTabCount(); i++) {
             int icon;
-            switch(i) {
-                case 0: icon = R.drawable.tab_business_infos; break;
-                case 1: icon = R.drawable.tab_business_reviews; break;
-                case 2: icon = R.drawable.tab_business_hairfies; break;
-                default: continue;
+            switch (i) {
+                case 0:
+                    icon = R.drawable.tab_business_infos;
+                    break;
+                case 1:
+                    icon = R.drawable.tab_business_reviews;
+                    break;
+                case 2:
+                    icon = R.drawable.tab_business_hairfies;
+                    break;
+                default:
+                    continue;
             }
             tabs.getTabAt(i).setIcon(icon);
         }
@@ -178,13 +196,27 @@ public class BusinessActivity extends AppCompatActivity implements BusinessInfoF
     }
 
     @Override
-    public void onTouchPhone(String phoneNumber) {
-        Log.d(Application.TAG, "Touch phone:"+phoneNumber);
+    public void onTouchPhone(final String phoneNumber) {
+        new AlertDialog.Builder(this).setTitle(String.format(getString(R.string.call_x), phoneNumber)).setNegativeButton(getString(R.string.no), null).setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                if (ActivityCompat.checkSelfPermission(BusinessActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    String[] permissions = {Manifest.permission.CALL_PHONE};
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(permissions, CALL_PERMISSION_REQUEST);
+                    }
+                    return;
+                }
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public void onTouchTimetable(Timetable timetable) {
-        Log.d(Application.TAG, "Touch timetable:"+timetable.toString());
+        Log.d(Application.TAG, "Touch timetable:" + timetable.toString());
 
     }
+
 }
