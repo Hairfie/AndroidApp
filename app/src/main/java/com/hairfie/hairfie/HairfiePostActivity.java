48d03d63cab8d3178a197d9650b04e72be88a96c
Stéphane JAIS <stephane@cantinasoftware.com>
2015-12-17 17:38:57 +0100

    tag selection

--- app/src/main/java/com/hairfie/hairfie/HairfiePostActivity.java
+++ app/src/main/java/com/hairfie/hairfie/HairfiePostActivity.java
@@ -1,6 +1,7 @@
 package com.hairfie.hairfie;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.hairfie.hairfie.models.ResultCallback;
+import com.hairfie.hairfie.models.Tag;
+import com.hairfie.hairfie.models.TagCategory;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -20,7 +26,8 @@ public class HairfiePostActivity extends AppCompatActivity {
     public static final String ARG_PICTUREFILEPATHS = "picture-file-paths";
 
     private List<File> mPictureFiles = new ArrayList<>();
-
+    private List<Tag> mSelectedTags = new ArrayList<>();
+    private ViewGroup mContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +63,34 @@ public class HairfiePostActivity extends AppCompatActivity {
             Application.getPicasso().load(mPictureFiles.get(1)).centerCrop().fit().into(picture2ImageView);
         else if (null != picture2ImageView)
             picture2ImageView.setVisibility(View.GONE);
+
+        mContainer = (ViewGroup)findViewById(R.id.container);
+
+        // Fill tag categories
+        TagCategory.ordered(new ResultCallback.Single<List<TagCategory>>() {
+            @Override
+            public void onComplete(@Nullable List<TagCategory> object, @Nullable ResultCallback.Error error) {
+                for (TagCategory category : object) {
+                    View categoryView = getLayoutInflater().inflate(R.layout.fragment_tag_category, mContainer, false);
+                    mContainer.addView(categoryView);
+                    TextView categoryTextView = (TextView) categoryView.findViewById(R.id.category);
+                    categoryTextView.setText(category.name);
+
+                    ViewGroup keywordContainer = (ViewGroup)categoryView.findViewById(R.id.tags);
+                    for (final Tag tag : category.tags) {
+                        TextView tagTextView = (TextView) getLayoutInflater().inflate(R.layout.fragment_tag, keywordContainer, false);
+                        tagTextView.setText(tag.name);
+                        tagTextView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                touchTag(tag, v);
+                            }
+                        });
+                        keywordContainer.addView(tagTextView);
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -81,4 +116,17 @@ public class HairfiePostActivity extends AppCompatActivity {
 
     }
 
+    private void touchTag(Tag tag, View v) {
+
+        if (v.isSelected()) {
+            v.setSelected(false);
+            mSelectedTags.remove(tag);
+        } else {
+            v.setSelected(true);
+            if (!mSelectedTags.contains(tag))
+                mSelectedTags.add(tag);
+        }
+
+
+    }
 }
--- app/src/main/java/com/hairfie/hairfie/models/Tag.java
+++ app/src/main/java/com/hairfie/hairfie/models/Tag.java
@@ -6,7 +6,22 @@ import com.squareup.okhttp.Call;
  * Created by stephh on 14/12/15.
  */
 public class Tag {
+    public String id;
     public String name;
     public int position;
     public TagCategory category;
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Tag))
+            return super.equals(o);
+
+        Tag other = (Tag) o;
+        return other.id.equals(id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
 }
