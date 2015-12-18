4aeae164d3790a73bf2d6c510a7fb94b387117fd
Stéphane JAIS <stephane@cantinasoftware.com>
2015-12-18 11:10:40 +0100

    choose salon & hairdresser.

---
+++ app/src/main/java/com/hairfie/hairfie/BusinessSearchActivity.java
@@ -0,0 +1,188 @@
+package com.hairfie.hairfie;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.hairfie.hairfie.models.Business;
+import com.hairfie.hairfie.models.BusinessMember;
+import com.hairfie.hairfie.models.GeoPoint;
+import com.hairfie.hairfie.models.ResultCallback;
+import com.squareup.okhttp.Call;
+
+import java.util.List;
+import java.util.Locale;
+
+public class BusinessSearchActivity extends AppCompatActivity {
+    public static final String RESULT_BUSINESS = "business";
+
+    EditText mQueryEditText;
+    EditText mLocationEditText;
+    View mProgressView;
+    GeoPoint mGeoPoint;
+    private BusinessRecyclerViewAdapter mAdapter = new BusinessRecyclerViewAdapter(new BusinessListFragment.OnListFragmentInteractionListener() {
+        @Override
+        public void onListFragmentInteraction(Business item) {
+            onTouchBusiness(item);
+        }
+
+
+    }
+    );
+
+    private void onTouchBusiness(Business item) {
+
+        Intent data = new Intent();
+        data.putExtra(RESULT_BUSINESS, item);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        TextView titleTextView = (TextView)findViewById(R.id.appbar_title);
+        if (null != titleTextView)
+            titleTextView.setText(R.string.choose_salon);
+
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.list);
+        recyclerView.setAdapter(mAdapter);
+
+        mProgressView = findViewById(R.id.progress);
+        mQueryEditText = (EditText) findViewById(R.id.query);
+        mLocationEditText = (EditText) findViewById(R.id.location);
+
+
+        mQueryEditText.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        mLocationEditText.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        TextView.OnEditorActionListener listener = new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    touchSearch();
+                    return true;
+                }
+                return false;
+            }
+        };
+        mLocationEditText.setOnEditorActionListener(listener);
+        mQueryEditText.setOnEditorActionListener(listener);
+
+        Location lastLocation = Application.getInstance().getLastLocation();
+        if (null != lastLocation)
+            mGeoPoint = new GeoPoint(lastLocation);
+
+        // If we have a last known location, search immediately
+        if (null != mGeoPoint)
+            touchSearch();
+
+    }
+
+    public void touchSearch() {
+        final CharSequence locationName = mLocationEditText.getText();
+        if (null == locationName || locationName.length() == 0) {
+            Location lastLocation = Application.getInstance().getLastLocation();
+            if (null != lastLocation)
+                mGeoPoint = new GeoPoint(lastLocation);
+
+            if (null == mGeoPoint) {
+                // We have no location
+                new AlertDialog.Builder(this).setTitle(getString(R.string.enter_explicit_location)).setPositiveButton(getString(R.string.ok), null).show();
+            } else {
+                refresh();
+            }
+        } else {
+
+            setSpinning(true);
+            GeoPoint.search(locationName.toString(), new ResultCallback.Single<GeoPoint>() {
+                @Override
+                public void onComplete(@Nullable GeoPoint object, @Nullable ResultCallback.Error error) {
+                    setSpinning(false);
+                    if (null == object) {
+                        // We have no location
+                        new AlertDialog.Builder(BusinessSearchActivity.this).setTitle(String.format(Locale.getDefault(), getString(R.string.cant_locate_name), locationName)).setPositiveButton(getString(R.string.ok), null).show();
+                        Log.e(Application.TAG, "Error requesting geolocation:" + (error.message != null ? error.message : "null"), error.cause);
+                    } else {
+                        mGeoPoint = object;
+                        refresh();
+                    }
+                }
+            });
+        }
+    }
+
+    Call mListBusinessesCall;
+    private void refresh() {
+
+        if (null == mGeoPoint)
+            return;
+
+        if (null != mListBusinessesCall && !mListBusinessesCall.isCanceled())
+            mListBusinessesCall.cancel();
+
+
+        mAdapter.setReferenceLocation(mGeoPoint.toLocation());
+
+        mAdapter.resetItems();
+
+        setSpinning(true);
+
+        final View noResults = findViewById(R.id.no_results);
+        if (null != noResults)
+            noResults.setVisibility(View.GONE);
+
+
+        CharSequence query = null != mQueryEditText.getText() ? mQueryEditText.getText() : "";
+        mListBusinessesCall = Business.listNearby(mGeoPoint, query.toString(), null, 100, new ResultCallback.Single<List<Business>>() {
+            @Override
+            public void onComplete(@Nullable List<Business> object, @Nullable ResultCallback.Error error) {
+                setSpinning(false);
+
+                if (null != error) {
+                    Log.e(Application.TAG, "Could not search businesses: " + (error.message != null ? error.message : "null"), error.cause);
+//                    finish();
+                    new AlertDialog.Builder(BusinessSearchActivity.this).setTitle(error.message).setPositiveButton(getString(R.string.ok), null).show();
+                    return;
+                }
+
+                if (null != object) {
+                    if (object.size() == 0 && null != noResults) {
+                        noResults.setVisibility(View.VISIBLE);
+                    }
+                    mAdapter.addItems(object);
+                }
+
+
+            }
+        });
+    }
+
+    public void touchClearLocation(View v) {
+        mLocationEditText.setText(null);
+        touchSearch();
+    }
+
+    public void setSpinning(boolean spinning) {
+        mProgressView.setVisibility(spinning ? View.VISIBLE : View.GONE);
+    }
+}
--- app/src/main/java/com/hairfie/hairfie/HairfiePostActivity.java
+++ app/src/main/java/com/hairfie/hairfie/HairfiePostActivity.java
@@ -1,18 +1,25 @@
 package com.hairfie.hairfie;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.hairfie.hairfie.models.Business;
+import com.hairfie.hairfie.models.BusinessMember;
 import com.hairfie.hairfie.models.ResultCallback;
 import com.hairfie.hairfie.models.Tag;
 import com.hairfie.hairfie.models.TagCategory;
@@ -24,10 +31,17 @@ import java.util.List;
 public class HairfiePostActivity extends AppCompatActivity {
 
     public static final String ARG_PICTUREFILEPATHS = "picture-file-paths";
+    private static final int REQUEST_CODE_CHOOSE_BUSINESS = 1002;
 
     private List<File> mPictureFiles = new ArrayList<>();
     private List<Tag> mSelectedTags = new ArrayList<>();
     private ViewGroup mContainer;
+
+    private Button mWhoButton;
+    private Button mWhereButton;
+
+    private Business mBusiness;
+    private BusinessMember mBusinessMember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +66,12 @@ public class HairfiePostActivity extends AppCompatActivity {
         for (int i = 0; i < pictureFilePaths.length; i++) {
             mPictureFiles.add(new File(pictureFilePaths[i]));
         }
+
+        // Who & Where
+        mWhoButton = (Button)findViewById(R.id.who);
+        mWhereButton = (Button)findViewById(R.id.where);
+        mWhoButton.setVisibility(View.GONE);
+
 
         // Fill image views
         ImageView picture1ImageView = (ImageView)findViewById(R.id.picture1);
@@ -109,10 +129,44 @@ public class HairfiePostActivity extends AppCompatActivity {
     }
 
     public void touchWho(View v) {
+        if (null == mBusiness || null == mBusiness.activeHairdressers || 0 == mBusiness.activeHairdressers.length)
+            return;
+
+        final CharSequence[] hairdressers = new CharSequence[mBusiness.activeHairdressers.length];
+        for (int i = 0; i < mBusiness.activeHairdressers.length; i++)
+            hairdressers[i] = mBusiness.activeHairdressers[i].getFullname();
+
+        new AlertDialog.Builder(this).setTitle(getString(R.string.choose_hairdresser)).setItems(hairdressers, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mBusinessMember = mBusiness.activeHairdressers[which];
+                mWhoButton.setText(hairdressers[which]);
+            }
+        }).show();
+
 
     }
 
     public void touchWhere(View v) {
+        new AlertDialog.Builder(this).setItems(R.array.where_choice, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+
+                    case 0:
+                        mWhereButton.setText(R.string.i_did_it);
+                        mBusiness = null;
+                        mWhoButton.setVisibility(View.GONE);
+                        break;
+
+                    default:
+                        Intent intent = new Intent(HairfiePostActivity.this, BusinessSearchActivity.class);
+                        startActivityForResult(intent, REQUEST_CODE_CHOOSE_BUSINESS);
+                        break;
+
+                }
+            }
+        }).show();
 
     }
 
@@ -128,5 +182,25 @@ public class HairfiePostActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (REQUEST_CODE_CHOOSE_BUSINESS == requestCode) {
+            if (RESULT_OK == resultCode && null != data) {
+                mBusiness = (Business)data.getParcelableExtra(BusinessSearchActivity.RESULT_BUSINESS);
+                mBusinessMember = null;
+                mWhoButton.setVisibility(View.GONE);
+                if (null != mBusiness) {
+                    mWhereButton.setText(mBusiness.name);
+                    if (mBusiness.activeHairdressers != null && mBusiness.activeHairdressers.length > 0) {
+                        mWhoButton.setVisibility(View.VISIBLE);
+                        mWhoButton.setText(R.string.who);
+                    }
+
+                }
+            }
+        }
     }
 }
